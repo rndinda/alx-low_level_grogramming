e93cbcdaf52b5a4829e85985223718fc339def29
rndinda <rndinda73@gmail.com>
2022-06-02 11:27:42 -0700

    My code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	        char output[] = "\"Programming is like building a multilingual puzzle";
+
+
+
+		        puts(output);
+
+			        return (0);
+
+} 
