619f5d80f4659263f18a445be890a84f1af2931c
rndinda <rndinda73@gmail.com>
2022-06-02 12:44:26 -0700

    My code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  char output[] = "\"Programming is like building a multilingual puzzle";
-  
-  puts(output);
-  return (0);
-} 
+	puts("\"Programming is like buildinga multilingual puzzle");
+	return (0);
+}
