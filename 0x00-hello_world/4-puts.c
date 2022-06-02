7047d7234372736bbc514e6ccc167d4f6a5f5e4a
Ndinda Rose <97664652+rndinda@users.noreply.github.com>
2022-06-02 22:12:55 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,7 @@
  */
 int main(void)
 {
-
-   char output[] = "\"Programming is like building a multilingual puzzle";
-  
-   puts(output);
-   return (0);
-
+ char output[] = "\"Programming is like building a multilingual puzzle";
+ puts(output);
+ return (0);
 } 
