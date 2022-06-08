f28442bb1693d13f0b626fa6525def5418849ca4
rndinda <rndinda73@gmail.com>
2022-06-07 22:12:59 -0700

    Prints lowercase values

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _islower - checks for the lowercase values.
+ * Return: 1 if c is lowercase , otherwise return 0.
+ */
+int _islower(int c)
+{
+	return (c >= 'a'; && c <= 'z');
+}
