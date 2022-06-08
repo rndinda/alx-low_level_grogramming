9b7d287d4bfd1de7f2f3528e280ce65be15bae4e
rndinda <rndinda73@gmail.com>
2022-06-08 13:08:11 -0700

    New 100 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,8 +24,7 @@ void print_times_table(int n)
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
+				} else if (z > 9)
 				{
 				       _putchar(',');
 				       _putchar(32);
@@ -37,10 +36,10 @@ void print_times_table(int n)
 				{
 				       if (y != 0)
 				       {
-				             _putchar(',');
-					     _putchar(32);
-					     _putchar(32);
-					     _putchar(32);
+					       _putchar(',');
+					       _putchar(32);
+					       _putchar(32);
+					       _putchar(32);
 				       }
 				       _putchar(z + '0');
 				}
