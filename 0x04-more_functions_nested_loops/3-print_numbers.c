9ad8e086c393fc298cb0c10ea4a60d3fcb9910a3
rndinda <rndinda73@gmail.com>
2022-06-09 13:01:03 -0700

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
  */
 void print_numbers(void)
 {
-	char c;
+	char b = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	while (b <= 9)
 	{
-		_putchar(c);
+		_putchar(b + '0');
+		b++;
 	}
 	_putchar('\n');
 }
