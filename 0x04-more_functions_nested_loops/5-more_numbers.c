b469d1d61fb7278320e118c830a6cb814f65abcc
rndinda <rndinda73@gmail.com>
2022-06-09 13:52:30 -0700

    Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - this function prints 10 times the numbers from 0 to 14
+ * 
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int b, c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
