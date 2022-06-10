541c49e20ed7d2057f91ed8078121e2253f1a628
rndinda <rndinda73@gmail.com>
2022-06-09 17:38:32 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints the square of the input
+ * @size: square dimensions
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size < 1)
+		_putchar('\n');
+}
