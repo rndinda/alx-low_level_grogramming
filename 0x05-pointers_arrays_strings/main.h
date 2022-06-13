be1000c64169a5a59c9c804adafbe122f542bd18
rndinda <rndinda73@gmail.com>
2022-06-13 12:02:16 -0700

    KeyGen

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <stdio.h>
 
 
@@ -12,3 +14,5 @@ void puts2(char *str);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void print_array(int *a, int n);
+#endif
