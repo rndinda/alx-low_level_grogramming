22cd7b3ea17675ed9966c5cd03f1b3017dfa5631
rndinda <rndinda73@gmail.com>
2022-06-16 02:19:11 -0700

    New 6th

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,30 @@
 #include "main.h"
 
 /**
- * cap_string - this function converts all letters
- * of a string to uppercase
- * @s: input the string
+ * cap_string - this function writes a string in uppercase
+ * @s: input string
  * Return: pointer to dest
  */
 char *cap_string(char *s)
 {
-	int index = 0;
+	int num = 0, i;
+	int sample_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (s[index])
+	if (*(s + num) >= 97 && *(s + num) <= 122)
+		*(s + num) = *(s + num) - 32;
+	num++;
+	while (*(s + num) != '\0')
 	{
-		while (!(s[index] >= 'a' && s[index] <= 'z'))
-			index++;
-		
-		if (s[index - 1] == ' ' ||
-				s[index - 1] == '\t' ||
-				s[index - 1] == '\n' ||
-				s[index - 1] == ',' ||
-				s[index - 1] == ';' ||
-				s[index - 1] == '.' ||
-				s[index - 1] == '!' ||
-				s[index - 1] == '?' ||
-				s[index - 1] == '"' ||
-				s[index - 1] == '(' ||
-				s[index - 1] == ')' ||
-				s[index - 1] == '{' ||
-				s[index - 1] == '}' ||
-				index == 0)
-				s[index] -= 32;
-
-		index++;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + num) == sample_words[i])
+			{
+				if ((*(s + (num + 1)) >= 97) && (*(s + (num + 1)) <= 122))
+					*(s(num + 1)) = *(s + (num + 1)) - 32;
+				break;
+			}
+		}
+		num++;
 	}
-
 	return (s);
 }
