d31e0465f31d218125f621e92f4b97e7774a878a
rndinda <rndinda73@gmail.com>
2022-06-16 03:00:20 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using r0t13
+ * @s: The string to be encoded
+ *
+ * Return: Pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	int x, y;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; a[y] != '\0'; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
