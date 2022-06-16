96db72b9fd657daaa2d39e3d337db2c2ffda4b43
rndinda <rndinda73@gmail.com>
2022-06-16 01:06:35 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - this function converts all letters
+ * of a string to uppercase
+ * @s: input the string
+ * Return: pointer to dest
+ */
+char *cap_string(char *s)
+{
+	int index = 0;
+
+	while (s[index])
+	{
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+
+		if (s[index - 1] == ' ' ||
+				s[index - 1] == '\t' ||
+				s[index - 1] == '\n' ||
+				s[index - 1] == ',' ||
+				s[index - 1] == ';' ||
+				s[index - 1] == '.' ||
+				s[index - 1] == '!' ||
+				s[index - 1] == '?' ||
+				s[index - 1] == '"' ||
+				s[index - 1] == '(' ||
+				s[index - 1] == ')' ||
+				s[index - 1] == '{' ||
+				s[index - 1] == '}' ||
+				index == 0)
+			        s[index] -= 32;
+
+		index++;
+	}
+
+	return (s);
+}
