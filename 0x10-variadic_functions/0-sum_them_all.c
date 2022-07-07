3f80c351240bd4bfc1ceac89d1d25aa5d4bace5a
rndinda <rndinda73@gmail.com>
2022-07-07 07:18:03 -0700

    Sum all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - computes sum of all its parameters.
+ * @n: the amount of arguments.
+ *
+ * Return: sum of the parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,23 @@
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * struct printer - New struct type defining a printer
+ * @symbol: A symbol representing a data type.
+ * @print: Function point to a function that prints a 
+ * datatype corresponding to symbol.
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
