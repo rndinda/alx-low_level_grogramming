646bfc638da4b031a489bb09af9ae50ed77b6c32
rndinda <rndinda73@gmail.com>
2022-07-09 02:47:26 -0700

    New 5th commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _strstr - locates a subtrsing
- * @haystack: string to check for needle
- * @needle: substring to findin haystack
+ * _strstr - locates a subtring
+ * @haystack: string to check the needle
+ * @needle: substring to find haystack
  *
- * Return: pointerto beginning of needle in haystack or NULL if no match
+ * Return: Pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0; j = 0;
+	unsigned int i = 0, j = 0;
 
 	while (haystack[i])
 	{
-		while (needle[ij] && (haystack[i] == needle[0]))
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
 			if (haystack[i + j] == needle[j])
 				j++;
