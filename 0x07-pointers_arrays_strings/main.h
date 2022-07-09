2cbcecfcd42ee48801d2ae38db75ad4f7e93b3c9
rndinda <rndinda73@gmail.com>
2022-07-09 04:38:12 -0700

    100-set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H
-_
-#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,6 +10,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char);
+void set_string(char **s, char *to);
 
 
 #endif
