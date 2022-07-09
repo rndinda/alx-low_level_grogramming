aaed18cf3790c0b495f1792f86c50082df1c4d9f
rndinda <rndinda73@gmail.com>
2022-07-09 04:14:28 -0700

    new

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef _MAIN_H_
-#define _MAIN_H_
+#define _MAIN_H
+_
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,5 +12,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char);
+
 
 #endif
