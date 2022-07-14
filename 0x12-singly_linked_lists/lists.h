14470aae47bf69a120b4ae9dad43c30163984443
rndinda <rndinda73@gmail.com>
2022-07-14 12:40:29 -0700

    TASK_3

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,5 +26,5 @@ size_t list_len(const list_t *h);
 int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 void free_list(list_t *head);
-
+list_t *add_node_end(list_t **head, const char *str);
 #endif
